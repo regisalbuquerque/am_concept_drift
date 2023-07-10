4b867aaab119eee252ab81063d012993296a1b2b
Regis Albuquerque <regis.albuquerque1@gmail.com>
2023-07-10 17:47:46 -0300

    Refatorando experimentos

--- evaluate/ttt.py
+++ evaluate/ttt.py
@@ -1,3 +1,7 @@
+import csv
+from datetime import datetime
+from river import metrics
+
 class TestThenTrain:
 
     def evaluate(model, dataset, metric):
@@ -7,3 +11,74 @@ class TestThenTrain:
             if y_pred is not None:
                 metric.update(y, y_pred)
         print(metric)
+
+    def evaluate_all(algos, bases, all_metrics):
+        
+        for bas in bases:
+
+            print(f'starting {bas}, at {datetime.now()}')
+
+            try:
+                f = open(bases[bas]['labels'], 'r')
+                y = []
+                for line in f.readlines():
+                    y.append(int(line))
+                f.close()
+                print(f'import {bas} labels, ok!')
+            except:
+                print('Y not found')
+            try:
+                f = open(bases[bas]['data'], 'r')
+                x = csv.DictReader(
+                    f,
+                    delimiter=' ',
+                    quoting=csv.QUOTE_NONNUMERIC,
+                    fieldnames=bases[bas]['fieldnames']
+                )
+                print(f'import {bas} data, ok!')
+            except:
+                print('X not found')
+
+
+            result = {}
+            for mod in algos:
+                _metric = {'y_pred': None}
+                for m in all_metrics:
+                    _m = getattr(metrics, m)()
+                    _metric.update({m: _m})
+                result.update({mod: _metric})
+
+            try:
+                w = open(f'results/{bas}.csv', 'w')
+                writer = csv.writer(w)
+                writer.writerow(['instance',
+                                'model', 
+                                'Accuracy',
+                                'Precision',
+                                'Recall',
+                                'F1',
+                                'GeometricMean'
+                                ])
+            except:
+                print('Result file can\'t be opened')
+
+            for i, _x in enumerate(x):
+                for mod in algos:
+                    model = algos[mod]
+
+                    # Test Then Train
+                    # Test
+                    result[mod]['y_pred'] = algos[mod].predict_one(_x)
+                    # Train
+                    algos[mod].learn_one(_x, y[i])
+
+                    if result[mod]['y_pred'] is not None:
+                        a = [i, mod]
+                        for m in all_metrics:
+                            result[mod][m].update(y[i], result[mod]['y_pred'])
+                            a.append(result[mod][m].get())
+
+                        writer.writerow(a)
+            print(f'finishing {bas}, at {datetime.now()}')
+            w.close()
+            f.close()
--- experimentos.py
+++ experimentos.py
@@ -1,17 +1,59 @@
 from evaluate import ttt
-from river import tree
-from river import metrics
-from river import stream
 
-modelEFDT = tree.ExtremelyFastDecisionTreeClassifier()
-metric_acuracia = metrics.Accuracy()
+from river import ensemble, forest, linear_model, neighbors, preprocessing, tree
 
-params = {
-    'converters': {'x': float, 'y': float, 'z': float},
-    'target': 'labels'
+
+algos = {
+    'ensemble': ensemble.LeveragingBaggingClassifier(preprocessing.StandardScaler() |
+                                                     linear_model.LogisticRegression()),
+    'tree': tree.ExtremelyFastDecisionTreeClassifier(),
+    'forest': forest.ARFClassifier(),
+    'neighbors': neighbors.KNNClassifier
 }
 
-dataset = stream.iter_csv("artificial/sea/sea.csv", **params)
+bases = {
+    'chess': {
+        'data': 'datasets/artificial/chess/transientChessboard.data',
+        'labels': 'datasets/artificial/chess/transientChessboard.labels',
+        'fieldnames': {'x', 'y'},
+    },
+    'rbf': {
+        'data': 'datasets/artificial/rbf/movingRBF.data',
+        'labels': 'datasets/artificial/rbf/movingRBF.labels',
+        'fieldnames': {'x0', 'x1', 'x2', 'x3', 'x4',
+                       'x5', 'x6', 'x7', 'x8', 'x9'},
+    },
+    'sea': {
+        'data': 'datasets/artificial/sea/SEA_training_data_xy.csv',
+        'labels': 'datasets/artificial/sea/SEA_training_class.csv',
+        'fieldnames': {'x', 'y'},
+    },
+    'squares': {
+        'data': 'datasets/artificial/movingSquares/movingSquares.data',
+        'labels': 'datasets/artificial/movingSquares/movingSquares.labels',
+        'fieldnames': {'x', 'y'},
+    # },
+    'poker': {
+        'data': 'datasets/realWord/poker/poker.data',
+        'labels': 'datasets/realWord/poker/poker.labels',
+        'fieldnames': {'x0', 'x1', 'x2', 'x3', 'x4',
+                       'x5', 'x6', 'x7', 'x8', 'x9'},
+    },
+    # 'weather': {
+        'data': 'datasets/realWord/weather/NEweather_data.csv',
+        'labels': 'datasets/realWord/weather/NEweather_class.csv',
+        'fieldnames': {'x0', 'x1', 'x2', 'x3', 
+                       'x4', 'x5', 'x6', 'x7'},
+    },
+}
+
+all_metrics = [
+    'Accuracy',
+    'Precision',
+    'Recall',
+    'F1',
+    'GeometricMean',
+]
 
 
-ttt.TestThenTrain.evaluate(modelEFDT, dataset, metric_acuracia)
+ttt.TestThenTrain.evaluate_all(algos, bases, all_metrics)
